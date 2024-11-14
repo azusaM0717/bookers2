0187e12919cc27616e0c72a2557d350c9b0c0591
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-14 05:44:03 +0000

    [finish]make-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
  
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'books/new'
+  get 'books/create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/destroy'
   devise_for :users
 root to: "homes#top"
 
