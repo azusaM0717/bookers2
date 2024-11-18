75dc66250d33d587f031f75e3ba859724662e857
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-18 03:22:59 +0000

    [start]bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
 has_many :books, dependent: :destroy
 has_one_attached :image
 
-def get_image(width,height)
-  unless image.attached?
+def get_image(width, height)
+  if image.attached?
+    image.variant(resize_to_limit: [width, height]).processed
+  else
     file_path = Rails.root.join('app/assets/images/sample-user1.jpg')
-    if image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-      image.variant(resize_to_limit: [width, height]).processed
-    end
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    image.variant(resize_to_limit: [width, height]).processed
   end
+end
+end
+
