6c3ffd4e697c4920eb03770b465c5f8f0f7b9b2b
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-18 17:43:57 +0000

    tired

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,20 +5,18 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] ="Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      render 'books/index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
-    unless @book.user_id == current_user.id
-      redirect_to books_path
-    end
   end
 
   def update
@@ -32,6 +30,7 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
 has_many :books, dependent: :destroy
 has_one_attached :image
 
+validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+validates :introduction, length: { maximum: 50 }
+
+
 def get_image(width, height)
   if image.attached?
     image.variant(resize_to_limit: [width, height]).processed
